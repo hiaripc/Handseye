4e2469c413e7c7838d55493f16fd131c71b8d14b
hiaripc <h.pizzinicavagna@gmail.com>
2022-09-30 19:16:15 +0200

    cleaning code

---
+++ TheSimpleApp/app/src/main/java/org/pytorch/demo/handseye/MainActivity.java
@@ -0,0 +1,286 @@
+// Copyright (c) 2020 Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+package org.pytorch.demo.handseye;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageAnalysisConfig;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Size;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.pytorch.IValue;
+import org.pytorch.LiteModuleLoader;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
+
+    private ImageView mImageView;
+    private ResultView mResultView;
+    private ProgressBar mProgressBar;
+    private Bitmap mBitmap = null;
+    private Module mModule = null;
+    private float mImgScaleX, mImgScaleY, mIvScaleX, mIvScaleY, mStartX, mStartY;
+    private long mLastAnalysisResultTime;
+    private ImageAnalysis imageAnalysis;
+    private Preview previewImage;
+    private LifecycleOwner lifecycleOwner = this;
+
+    public static String assetFilePath(Context context, String assetName) throws IOException {
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.imageView);
+        mImageView.setVisibility(View.INVISIBLE);
+        mResultView = findViewById(R.id.resultView);
+        mResultView.setVisibility(View.INVISIBLE);
+
+        //TODO Change it to a more appealing floating button (cancel option no more needed)
+        final FloatingActionButton buttonSelect = findViewById(R.id.selectButton);
+        buttonSelect.setOnClickListener((View v) -> {
+                mResultView.setVisibility(View.INVISIBLE);
+                final CharSequence[] options = { "Choose from Photos", "Take Picture", "Cancel" };
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("New Test Image");
+
+                builder.setItems(options, (DialogInterface dialog, int item) ->{
+                        if (options[item].equals("Take Picture")) {
+                            //todo would be great to find a way to just capture clicking the liveButton once..
+                            CameraX.unbind(previewImage,imageAnalysis);
+                            Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(takePicture, 0);
+                        }
+                        else if (options[item].equals("Choose from Photos")) {
+                            CameraX.unbind(previewImage,imageAnalysis);
+                            Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                            startActivityForResult(pickPhoto , 1);
+                        }
+                        else if (options[item].equals("Cancel")) {
+                            dialog.dismiss();
+                        }
+                });
+                builder.show();
+        });
+
+        setupCameraX();
+        CameraX.bindToLifecycle(lifecycleOwner,previewImage);
+
+        final Button buttonLive = findViewById(R.id.liveButton);
+        buttonLive.setOnClickListener((View v) -> {
+            if(!CameraX.isBound(imageAnalysis) && !CameraX.isBound(previewImage))
+                CameraX.bindToLifecycle(lifecycleOwner,previewImage,imageAnalysis);
+            if (!CameraX.isBound(imageAnalysis)) {
+                //setupCameraX();
+                CameraX.bindToLifecycle(lifecycleOwner,imageAnalysis);
+            }
+
+
+        });
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        try {
+            mModule = LiteModuleLoader.load(MainActivity.assetFilePath(getApplicationContext(), "fine-tuned.torchscript.ptl"));
+            BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open("alphabet.txt")));
+            String line;
+            List<String> classes = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                classes.add(line);
+            }
+            PrePostProcessor.mClasses = new String[classes.size()];
+            classes.toArray(PrePostProcessor.mClasses);
+        } catch (IOException e) {
+            Log.e("Object Detection", "Error reading assets", e);
+            finish();
+        }
+    }
+    //TODO divide the bind and the setup so it can be called again
+    //there's a problem with textureview, making it crashing
+    private void setupCameraX() {
+        ImageAnalyser imageAnalyser = new ImageAnalyser(mResultView, getApplicationContext());
+        mResultView.setVisibility(View.VISIBLE);
+
+        final TextureView textureView = ((ViewStub) findViewById(R.id.object_detection_texture_view_stub))
+                .inflate()
+                .findViewById(R.id.object_detection_texture_view);
+        final PreviewConfig previewConfig = new PreviewConfig.Builder().build();
+        previewImage = new Preview(previewConfig);
+        previewImage.setOnPreviewOutputUpdateListener(output -> textureView.setSurfaceTexture(output.getSurfaceTexture()));
+
+        final ImageAnalysisConfig imageAnalysisConfig =
+                new ImageAnalysisConfig.Builder()
+                        .setTargetResolution(new Size(480, 640))
+                        .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+                        .build();
+        //.setCallbackHandler(mBackgroundHandler)
+        imageAnalysis = new ImageAnalysis(imageAnalysisConfig);
+        imageAnalysis.setAnalyzer((image, rotationDegrees) -> {
+            if (SystemClock.elapsedRealtime() - mLastAnalysisResultTime < 500) {
+                return;
+            }
+
+            final ImageAnalyser.AnalysisResult result = imageAnalyser.analyzeImage(image, rotationDegrees);
+            if (result != null) {
+                mLastAnalysisResultTime = SystemClock.elapsedRealtime();
+                runOnUiThread(() -> applyToUiAnalyzeImageResult(result));
+            }
+        });
+
+        lifecycleOwner = this;
+        //CameraX.bindToLifecycle(this, previewAnalysis, imageAnalysis);
+
+    }
+
+    protected void applyToUiAnalyzeImageResult(ImageAnalyser.AnalysisResult result) {
+        mResultView.setResults(result.mResults);
+        mResultView.invalidate();
+    }
+
+    protected void startDetection(){
+        mProgressBar.setVisibility(ProgressBar.VISIBLE);
+
+        mImgScaleX = (float)mBitmap.getWidth() / PrePostProcessor.mInputWidth;
+        mImgScaleY = (float)mBitmap.getHeight() / PrePostProcessor.mInputHeight;
+
+        mIvScaleX = (mBitmap.getWidth() > mBitmap.getHeight() ? (float)mImageView.getWidth() / mBitmap.getWidth() : (float)mImageView.getHeight() / mBitmap.getHeight());
+        mIvScaleY  = (mBitmap.getHeight() > mBitmap.getWidth() ? (float)mImageView.getHeight() / mBitmap.getHeight() : (float)mImageView.getWidth() / mBitmap.getWidth());
+
+        mStartX = (mImageView.getWidth() - mIvScaleX * mBitmap.getWidth())/2;
+        mStartY = (mImageView.getHeight() -  mIvScaleY * mBitmap.getHeight())/2;
+
+        Thread thread = new Thread(MainActivity.this);
+        thread.start();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_CANCELED) {
+            //Replacing liveView with mImageView
+            mImageView.setVisibility(View.VISIBLE);
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        mBitmap = (Bitmap) data.getExtras().get("data");
+                        Matrix matrix = new Matrix();
+                        //matrix.postRotate(90.0f);
+                        mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
+                        mImageView.setImageBitmap(mBitmap);
+                        startDetection();
+                    }
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                mBitmap = BitmapFactory.decodeFile(picturePath);
+                                Matrix matrix = new Matrix();
+                                //matrix.postRotate(90.0f);
+                                mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
+                                mImageView.setImageBitmap(mBitmap);
+                                cursor.close();
+                            }
+                        }
+                        startDetection();
+                    }
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(mBitmap, PrePostProcessor.mInputWidth, PrePostProcessor.mInputHeight, true);
+        final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(resizedBitmap, PrePostProcessor.NO_MEAN_RGB, PrePostProcessor.NO_STD_RGB);
+        IValue[] outputTuple = mModule.forward(IValue.from(inputTensor)).toTuple();
+        final Tensor outputTensor = outputTuple[0].toTensor();
+        final float[] outputs = outputTensor.getDataAsFloatArray();
+        final ArrayList<Result> results =  PrePostProcessor.outputsToNMSPredictions(outputs, mImgScaleX, mImgScaleY, mIvScaleX, mIvScaleY, mStartX, mStartY);
+
+        runOnUiThread(() -> {
+            mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+            mResultView.setResults(results);
+            mResultView.invalidate();
+            mResultView.setVisibility(View.VISIBLE);
+        });
+    }
+}
